1507d93c8670171a2a6b3fdbdfde70a0416b210a
sanjoy <sanjoy120@gmail.com>
2015-08-26 20:31:37 +0600

    loginactivity

---
+++ app/src/main/java/com/pixisolutions/loyaltymanager/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.pixisolutions.loyaltymanager;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by sanjoy on 8/26/15.
+ */
+public class LoginActivity extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
